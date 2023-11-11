7c063260f41e53c1e0098223314e19b19c4923ae
SerjeiMikailov <felipemaders8@gmail.com>
2023-11-10 23:34:57 -0300

    gg

---
+++ gui/MyForm.h
@@ -0,0 +1,129 @@
+#pragma once
+
+#include "cstdlib"
+
+namespace gui {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::ProgressBar^ progressBar1;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->progressBar1 = (gcnew System::Windows::Forms::ProgressBar());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(126, 94);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(65, 13);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Install Merrli ";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(110, 132);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(103, 43);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"Install";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// progressBar1
+			// 
+			this->progressBar1->ForeColor = System::Drawing::Color::Lime;
+			this->progressBar1->Location = System::Drawing::Point(110, 191);
+			this->progressBar1->Name = L"progressBar1";
+			this->progressBar1->Size = System::Drawing::Size(100, 23);
+			this->progressBar1->TabIndex = 2;
+			this->progressBar1->Visible = false;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(321, 277);
+			this->Controls->Add(this->progressBar1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"Merrli_Installer";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e)
+	{
+		progressBar1->Visible = true;
+
+		for (int i = 0; i <= 100; i++)
+		{
+			progressBar1->Value = i;
+
+			System::Threading::Thread::Sleep(40);
+		}
+
+		// system("git clone https://github.com/SerjeiMikailov/merrli.git");
+
+		progressBar1->Visible = false;
+
+		Application::Exit();
+	}
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) 
+	{
+		
+	}
+	};
+}
